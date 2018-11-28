a78e3d402360087d9cce6e8cc5683d8915edecf1
rmgarcia <rockdrigo.mtz@gmail.com>
2018-11-28 09:11:35 -0600

    ownership, move rule

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    move_sample();
+}
+
+
+fn move_sample() {
+    let s1 = String::from("hello");
+    let s2 = s1;
+    println!("{}, world!", s2);
+    //s1 was moved to variable s2
+    //println!("{}, world!", s2);
 }
