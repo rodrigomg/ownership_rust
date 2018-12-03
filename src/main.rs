6274203154388c8d352b5aca8e0ec22e09b27beb
rmgarcia <rockdrigo.mtz@gmail.com>
2018-12-03 16:02:54 -0600

    ownership, clone rule

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 fn main() {
     move_sample();
+    clone_sample();
 }
 
 
@@ -9,4 +10,10 @@ fn move_sample() {
     println!("{}, world!", s2);
     //s1 was moved to variable s2
     //println!("{}, world!", s2);
+}
+
+fn clone_sample() {
+    let s1 = String::from("hello");
+    let s2 = s1.clone();
+    println!("s1 = {}, s2 = {}", s1, s2);
 }
